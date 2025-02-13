879e62eeee876182bc9fe417826666990a8a4ad8
ikyrie <mah.c5r@gmail.com>
2025-02-13 17:34:41 -0300

    video4.3

--- lib/viewmodels/checkout_viewmodel.dart
+++ lib/viewmodels/checkout_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
 import 'package:panucci_ristorante/models/item.dart';
 import 'package:panucci_ristorante/services/printing_service.dart';
+import 'package:panucci_ristorante/utils/printer_settings_utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CheckoutViewmodel {
@@ -11,15 +12,12 @@ class CheckoutViewmodel {
 
   Future<List<int>> _prepareReceipt(List<Item> items, double total) async {
     final SharedPreferences preferences = await SharedPreferences.getInstance();
-    int paperSize = preferences.getInt("paperSize")!;
-    int textSize = preferences.getInt("textSize")!;
-
-    print(paperSize);
-    print(textSize);
+    PaperSize paperSize = PaperSizeUtils.getPaperSize(preferences.getInt("paperSize") ?? 1);
+    PosTextSize textSize = TextSizeUtils.getTextSize(preferences.getInt("textSize") ?? 1);
 
     List<int> bytes =[];
     CapabilityProfile profile = await CapabilityProfile.load();
-    Generator generator = Generator(PaperSize.mm58, profile);
+    Generator generator = Generator(paperSize, profile);
     bytes += generator.reset();
     bytes += generator.feed(2);
     bytes += generator.text("Panucci Ristorante", styles: PosStyles(
@@ -31,11 +29,11 @@ class CheckoutViewmodel {
     bytes += generator.qrcode("https://www.panucci.com.br", size: QRSize.size8);
     bytes += generator.feed(1);
     for (Item item in items) {
-      bytes += generator.row([PosColumn(text: item.nome, width: 6, styles: PosStyles(align: PosAlign.left)), PosColumn(text: item.preco.toStringAsFixed(2), width: 6, styles: PosStyles(align: PosAlign.right))]);
+      bytes += generator.row([PosColumn(text: item.nome, width: 6, styles: PosStyles(align: PosAlign.left, height: textSize)), PosColumn(text: item.preco.toStringAsFixed(2), width: 6, styles: PosStyles(align: PosAlign.right, height: textSize))]);
     }
     bytes += generator.reset();
     bytes += generator.hr();
-    bytes += generator.row([PosColumn(text: "Total", width: 6, styles: PosStyles(align: PosAlign.left)),PosColumn(text: total.toStringAsFixed(2), width: 6, styles: PosStyles(align: PosAlign.right))]);
+    bytes += generator.row([PosColumn(text: "Total", width: 6, styles: PosStyles(align: PosAlign.left, height: textSize)),PosColumn(text: total.toStringAsFixed(2), width: 6, styles: PosStyles(align: PosAlign.right, height: textSize))]);
     bytes += generator.feed(3);
 
     return bytes;
