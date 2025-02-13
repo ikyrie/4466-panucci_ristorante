5ed34ad2993586cc64e0f99da70f15a1da012d14
ikyrie <mah.c5r@gmail.com>
2025-02-13 15:08:33 -0300

    video2.3

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -77,7 +77,98 @@ class Checkout extends StatelessWidget {
                       List<int> bytes = [];
                       CapabilityProfile profile = await CapabilityProfile.load();
                       Generator generator = Generator(PaperSize.mm58, profile);
-                      bytes += generator.text("Panucci Ristorante");
+
+                        // Using default profile
+                        //bytes += generator.setGlobalFont(PosFontType.fontA);
+                        bytes += generator.reset();
+                        bytes += generator.text(
+                            'Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ',
+                            styles: PosStyles());
+                        bytes += generator.text(
+                            'Special 1: ñÑ àÀ èÈ éÉ üÜ çÇ ôÔ',
+                            styles: PosStyles(codeTable: 'CP1252'));
+                        bytes += generator.text(
+                          'Special 2: blåbærgrød',
+                          styles: PosStyles(codeTable: 'CP1252'),
+                        );
+
+                        bytes += generator.text('Bold text',
+                            styles: PosStyles(bold: true));
+                        bytes += generator.text('Reverse text',
+                            styles: PosStyles(reverse: true));
+                        bytes += generator.text('Underlined text',
+                            styles: PosStyles(underline: true), linesAfter: 1);
+                        bytes += generator.text('Align left',
+                            styles: PosStyles(align: PosAlign.left));
+                        bytes += generator.text('Align center',
+                            styles: PosStyles(align: PosAlign.center));
+                        bytes += generator.text('Align right',
+                            styles: PosStyles(align: PosAlign.right),
+                            linesAfter: 1);
+
+                        bytes += generator.row([
+                          PosColumn(
+                            text: 'col3',
+                            width: 3,
+                            styles: PosStyles(
+                                align: PosAlign.center, underline: true),
+                          ),
+                          PosColumn(
+                            text: 'col6',
+                            width: 6,
+                            styles: PosStyles(
+                                align: PosAlign.center, underline: true),
+                          ),
+                          PosColumn(
+                            text: 'col3',
+                            width: 3,
+                            styles: PosStyles(
+                                align: PosAlign.center, underline: true),
+                          ),
+                        ]);
+
+                        //barcode
+                        final List<int> barData = [
+                          1,
+                          2,
+                          3,
+                          4,
+                          5,
+                          6,
+                          7,
+                          8,
+                          9,
+                          0,
+                          4
+                        ];
+                        bytes += generator.barcode(Barcode.upcA(barData));
+
+                        //QR code
+                        bytes += generator.qrcode('example.com');
+
+                        bytes += generator.text(
+                          'Text size 50%',
+                          styles: PosStyles(
+                            fontType: PosFontType.fontB,
+                          ),
+                        );
+                        bytes += generator.text(
+                          'Text size 100%',
+                          styles: PosStyles(
+                            fontType: PosFontType.fontA,
+                          ),
+                        );
+                        bytes += generator.text(
+                          'Text size 200%',
+                          styles: PosStyles(
+                            height: PosTextSize.size2,
+                            width: PosTextSize.size2,
+                          ),
+                        );
+
+                        bytes += generator.feed(2);
+                        //bytes += generator.cut();
+
                       await PrintingService.printReceipt(bytes);
                     },
                   ),
